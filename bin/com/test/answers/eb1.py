acdcb4818eb24544b5d81184083bb51072208be7
Bryan Laryea <bry_lar@yahoo.com>
2021-03-21 12:17:42 +0000

    second commit

---
+++ bin/com/test/answers/eb1.py
@@ -0,0 +1,24 @@
+from random import randint
+
+
+def foo(length):
+    generated = []
+    while len(generated) < length:
+        r = randint(1, length)
+        if r not in generated:
+            generated.append(r)
+
+    return generated
+
+generated = []
+
+def foo2(length):
+    if len(generated) < length:
+        r = randint(1, length)
+        if r not in generated:
+            print(r)
+            generated.append(r)
+        foo2(length);
+
+x = foo2(10)
+
